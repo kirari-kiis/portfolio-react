35bb25174fe05bc8881db21d032c73434f49a4b2
kirari_kiis <kirari0709@icloud.com>
2020-02-27 20:56:18 +0900

    レスポンシブtest4

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ class Portfolio extends Component{
 const LinkButton = (props) => {
     return (
         <div className="link">
-            <a id='homelink' href='#back' onClick={() => props.actionSlide(0)}><img src={homeUrl} alt='' /></a><br/>
+            <a id='homelink' href='#root' onClick={() => props.actionSlide(0)}><img src={homeUrl} alt='' /></a><br/>
             <a id='aboutlink' href='#profile' onClick={() => props.actionSlide(1)}><img src={aboutUrl} alt='' /></a><br/>
             <a id='workslink' href='#works' onClick={() => props.actionSlide(2)}><img src={worksUrl} alt='' /></a><br/>
             <a id='contactlink' href='#contact' onClick={() => props.actionSlide(3)}><img src={contactUrl} alt='' /></a><br/>
